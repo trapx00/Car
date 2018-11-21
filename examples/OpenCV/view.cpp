41895772f85bc84a11e3b2cdce8a792949654487
surevil <445073309@qq.com>
2018-11-21 20:01:35 +0800

    add cv about red square but

--- examples/OpenCV/view.cpp
+++ examples/OpenCV/view.cpp
@@ -28,7 +28,9 @@ using namespace std;
 
 void drawLine(Mat &picture, Point startPoint, Point endPoint);
 
-double calculateDistance(Point point1, Point point2);
+void drawUnimportantLine(Mat &picture, Point startPoint, Point endPoint)
+
+    double calculateDistance(Point point1, Point point2);
 
 double calcAngle(double x1, double y1, double x2, double y2);
 
@@ -43,17 +45,19 @@ const int CANNY_UPPER_BOUND = 250;
 const int HOUGH_THRESHOLD = 150;
 const int CV_CAP_DROP_FPS = 24;
 
-struct Line {
-public:
+struct Line
+{
+  public:
     double length;
     double k;
     double b;
-    
+
     Line(double length, double k, double b) : length(length), k(k), b(b) {}
 };
 
 // returns angle
-void analysePicture(Mat imag, double& angle) {
+void analysePicture(Mat imag, double &angle)
+{
     //    Rect rect(0, imag.rows / 2, imag.cols, imag.rows / 2 - 72);
     //    Mat result = imag(rect);
     //    Mat element = getStructuringElement(MORPH_RECT, Size(2 * erosion_size + 1, 2 * erosion_size + 1),
@@ -63,108 +67,196 @@ void analysePicture(Mat imag, double& angle) {
     //    result = imag.clone();
     //    erode(result, result, element);
     //    dilate(result, result, element);
-    resizeImage(imag);
+    // resizeImage(imag);
     imag = imread("image.jpg", 0);
+
+    int iLowH = 0;
+    int iHighH = 10;
+
+    int iLowS = 43;
+    int iHighS = 255;
+
+    int iLowV = 46;
+    int iHighV = 255;
+
+    Mat imgHSV;
+    vector<Mat> hsvSplit;
+    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+    //因为我们读取的是彩色图，直方图均衡化需要在HSV空间做
+    split(imgHSV, hsvSplit);
+    equalizeHist(hsvSplit[2], hsvSplit[2]);
+    merge(hsvSplit, imgHSV);
+    Mat imgThresholded;
+
+    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+    //开操作 (去除一些噪点)
+    Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+    morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
+
+    //闭操作 (连接一些连通域)
+    morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
+
+    vector<vector<Point>> contours;
+    findContours(imgThresholded, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE); //找轮廓
+
+    vector<vector<Point>> contours1;
+    Point squareCenter = Point(0, 0);
+    for (int i = 0; i < contours.size(); ++i)
+    {
+        if (contours[i].size() == 4)
+        {
+            squareCenter.x = (contours[i][0].x + contours[i][1].x + contours[i][2].x + contours[i][3].x) / 4;
+            squareCenter.y = (contours[i][0].y + contours[i][1].y + contours[i][2].y + contours[i][3].y) / 4;
+        }
+    }
+#ifdef _DEBUG
+    drawContours(hole, contours1, -1, Scalar(255, 0, 0), CV_FILLED); //在遮罩图层上，用白色像素填充轮廓，得到MASK
+#endif
+
     Mat result = imag.clone();
     Canny(result, result, 50, 250, 3);
     std::vector<Vec4i> lines;
     std::vector<Line> leftLineTuples;
     std::vector<Line> rightLineTuples;
-    
+
     cv::HoughLinesP(result, lines, 1, CV_PI / 180, 70, 30, 10);
-    
-    for (int i = 0; i < lines.size(); i++) {
+
+    for (int i = 0; i < lines.size(); i++)
+    {
         double delta_x = lines[i][2] - lines[i][0];
         double delta_y = lines[i][3] - lines[i][1];
         double length = sqrt(delta_x * delta_x + delta_y * delta_y);
         double k = delta_y / delta_x;
         double b = lines[i][1] - k * lines[i][0];
-        if (k > 0) {
+        if (k > 0)
+        {
+#ifdef _DEBUG
+            Point rightEndPoint1 = Point(lines[i][0], lines[i][1]);
+            Point rightStartPoint1 = Point(lines[i][2], lines[i][3]);
+            drawUnimportantLine(imag_real, rightEndPoint1, rightStartPoint1);
+#endif
             rightLineTuples.push_back(Line(length, k, b));
-        } else if (k < 0) {
+        }
+        else if (k < 0)
+        {
+#ifdef _DEBUG
+            Point leftEndPoint1 = Point(lines[i][0], lines[i][1]);
+            Point leftStartPoint1 = Point(lines[i][2], lines[i][3]);
+            drawUnimportantLine(imag_real, leftEndPoint1, leftStartPoint1);
+#endif
             leftLineTuples.push_back(Line(length, k, b));
         }
     }
-    
-    if (rightLineTuples.size() <= 0 && leftLineTuples.size() <= 0) {
-#ifdef _DEBUG
-            imshow("Main Window", imag);
 
-            waitKey(1);
-#endif
-        return;
-    }else if(rightLineTuples.size()>=0&& leftLineTuples.size() <= 0){
-        angle=1;
-        return;
-    }else if(rightLineTuples.size() <= 0 && leftLineTuples.size() >= 0){
-        angle=-1;
+    if (rightLineTuples.size() <= 0 && leftLineTuples.size() <= 0)
+    {
         return;
     }
-    
+    else if (rightLineTuples.size() >= 0 && leftLineTuples.size() <= 0)
+    {
+        angle = 1;
+        return;
+    }
+    else if (rightLineTuples.size() <= 0 && leftLineTuples.size() >= 0)
+    {
+        angle = -1;
+        return;
+    }
+
     int maxRightLengthIndex = 0;
-    int smallestRightAngleIndex=0;
-    int secondRightLengthIndex=-1;
+    int smallestRightAngleIndex = 0;
+    int secondRightLengthIndex = -1;
     double maxLength = 0;
-    double smallestAngle=10;
-    for (int i = 0; i < rightLineTuples.size(); i++) {
-        if (maxLength < rightLineTuples[i].length) {
+    double smallestAngle = 10;
+    for (int i = 0; i < rightLineTuples.size(); i++)
+    {
+        if (maxLength < rightLineTuples[i].length)
+        {
             maxLength = rightLineTuples[i].length;
-            secondRightLengthIndex=maxRightLengthIndex;
-	    maxRightLengthIndex = i;
+            secondRightLengthIndex = maxRightLengthIndex;
+            maxRightLengthIndex = i;
         }
-	if(smallestAngle>rightLineTuples[i].k){
-	    smallestAngle=rightLineTuples[i].k;
-	    smallestRightAngleIndex=i;
-	}
+        if (smallestAngle > rightLineTuples[i].k)
+        {
+            smallestAngle = rightLineTuples[i].k;
+            smallestRightAngleIndex = i;
+        }
+    }
+    if (maxRightLengthIndex != smallestRightAngleIndex)
+    {
+        if (secondRightLengthIndex != -1)
+        {
+            maxRightLengthIndex = secondRightLengthIndex;
+        }
+    }
 
-    }
-    if(maxRightLengthIndex!=smallestRightAngleIndex){
-	if(secondRightLengthIndex!=-1){
-	    maxRightLengthIndex=secondRightLengthIndex;
-	}
-    }
-    
     int maxLeftLengthIndex = 0;
-    int smallestLeftAngleIndex=0;
-    int secondLeftLengthIndex=-1;
+    int smallestLeftAngleIndex = 0;
+    int secondLeftLengthIndex = -1;
     maxLength = 0;
-    smallestAngle=0;
-    for (int i = 0; i < leftLineTuples.size(); i++) {
-        if (maxLength < leftLineTuples[i].length) {
+    smallestAngle = -10;
+    for (int i = 0; i < leftLineTuples.size(); i++)
+    {
+        if (maxLength < leftLineTuples[i].length)
+        {
             maxLength = leftLineTuples[i].length;
+            secondRightLengthIndex = maxRightLengthIndex;
             maxLeftLengthIndex = i;
         }
+        if (smallestAngle < rightLineTuples[i].k)
+        {
+            smallestAngle = rightLineTuples[i].k;
+            smallestRightAngleIndex = i;
+        }
     }
-    
+    if (maxLeftLengthIndex != smallestLeftAngleIndex)
+    {
+        if (secondLeftLengthIndex != -1)
+        {
+            maxLeftLengthIndex = secondLeftLengthIndex;
+        }
+    }
+
+    if (contours1.size() >= 1)
+    {
+        if(squareCenter.x<(result.columns/2){
+            leftLineTuples[maxLeftLengthIndex].b = leftLineTuples[maxLeftLengthIndex].b + squareCenter.x * 2 / k;
+        }else{
+            rightLineTuples[maxRightLengthIndex] = rightLineTuples[maxRightLengthIndex].b + (result.columns - squareCenter.x) * 2 / k;
+        }
+    }
+
     Line rightMaxLengthLine = rightLineTuples[maxRightLengthIndex];
     Line leftMaxLengthLine = leftLineTuples[maxLeftLengthIndex];
     double joinX = (leftMaxLengthLine.b - rightMaxLengthLine.b) / (rightMaxLengthLine.k - leftMaxLengthLine.k);
     double joinY = leftMaxLengthLine.k * joinX + leftMaxLengthLine.b;
-    
+
     double deltaAngle = calcAngle(joinX, joinY, result.cols / 2, result.rows);
-    
+
     // returns values
     angle = deltaAngle;
-    
-    #ifdef _DEBUG
+
+#ifdef _DEBUG
     //画线
-     double rightLineX = -rightMaxLengthLine.b / rightMaxLengthLine.k;
-     double rightLineY = rightMaxLengthLine.k * imag.cols + rightMaxLengthLine.b;
-     Point rightEndPoint = Point(static_cast<int>(rightLineX), 0);
-     Point rightStartPoint = Point(imag.cols, static_cast<int>(rightLineY));
-     
-     double leftLineX = -leftMaxLengthLine.b / leftMaxLengthLine.k;
-     double leftLineY = leftMaxLengthLine.b;
-     Point leftEndPoint = Point(static_cast<int>(leftLineX), 0);
-     Point leftStartPoint = Point(0, static_cast<int>(leftLineY));
-     
-     drawLine(result, leftStartPoint, leftEndPoint);
-     drawLine(result, rightStartPoint, rightEndPoint);
-    
-    imshow("Main Window", result);
+    double rightLineX = -rightMaxLengthLine.b / rightMaxLengthLine.k;
+    double rightLineY = rightMaxLengthLine.k * imag.cols + rightMaxLengthLine.b;
+    Point rightEndPoint = Point(static_cast<int>(rightLineX), 0);
+    Point rightStartPoint = Point(imag.cols, static_cast<int>(rightLineY));
+
+    double leftLineX = -leftMaxLengthLine.b / leftMaxLengthLine.k;
+    double leftLineY = leftMaxLengthLine.b;
+    Point leftEndPoint = Point(static_cast<int>(leftLineX), 0);
+    Point leftStartPoint = Point(0, static_cast<int>(leftLineY));
+
+    drawLine(imag_real, leftStartPoint, leftEndPoint);
+    drawLine(imag_real, rightStartPoint, rightEndPoint);
+
+    imshow("Main Window", imag_real);
     waitKey(1);
-    #endif
-    
+#endif
+
     //    vector<Point2f> corners(4);
     //    corners[0] = leftStartPoint;
     //    corners[1] = rightStartPoint;
@@ -176,12 +268,13 @@ void analysePicture(Mat imag, double& angle) {
     //    corners_trans[2] = Point2f(leftStartPoint.x, 10);
     //    corners_trans[3] = Point2f(rightStartPoint.x, 10);
     //    Mat transform = getPerspectiveTransform(corners, corners_trans);
-    
+
     //    warpPerspective(imag, result, transform, cv::Size(imag.cols + 100, imag.rows), cv::INTER_LINEAR);
     //    imwrite("result.jpg", imag);
 }
 
-double calcAngle(double x1, double y1, double x2, double y2) {
+double calcAngle(double x1, double y1, double x2, double y2)
+{
     double angle_temp;
     double xx, yy;
     xx = x2 - x1;
@@ -190,11 +283,12 @@ double calcAngle(double x1, double y1, double x2, double y2) {
         angle_temp = PI / 2.0;
     else
         angle_temp = atan(yy / xx);
-    
+
     return angle_temp;
 }
 
-void resizeImage(Mat frame) {
+void resizeImage(Mat frame)
+{
     Rect rect(0, frame.rows / 2, frame.cols, frame.rows / 2 - 72);
     Mat image_roi = frame(rect);
     Mat mask = image_roi.clone();
@@ -202,7 +296,8 @@ void resizeImage(Mat frame) {
     imwrite("image.jpg", image_roi);
 }
 
-void getOneShot() {
+void getOneShot()
+{
     // 获取视频文件
     VideoCapture cap("video.avi");
     // 获取视频总帧数
@@ -215,14 +310,16 @@ void getOneShot() {
     imwrite("image.jpg", image_roi);
 }
 
-void record() {
+void record()
+{
     VideoCapture capture(CAM_PATH);
     VideoWriter write;
     //If this fails, try to open as a video camera, through the use of an integer param
-    if (!capture.isOpened()) {
+    if (!capture.isOpened())
+    {
         capture.open(atoi(CAM_PATH.c_str()));
     }
-    
+
     int dWidth = static_cast<int>(capture.get(CAP_PROP_FRAME_WIDTH)); //the width of frames of the video
     int dHeight = static_cast<int>(capture.get(CAP_PROP_FRAME_HEIGHT));
     Size S(dWidth, dHeight);
@@ -231,15 +328,17 @@ void record() {
     capture.get(CV_CAP_DROP_FPS);
     string outFile = "./video.avi";
     write.open(outFile, VideoWriter::fourcc('M', 'J', 'P', 'G'), CV_CAP_DROP_FPS, S, true);
-    
+
     bool stop = false;
     Mat frame;
-    while (!stop) {
+    while (!stop)
+    {
         if (!capture.read(frame))
             break;
         imshow("Video", frame);
         write.write(frame);
-        if (waitKey(10) > 0) {
+        if (waitKey(10) > 0)
+        {
             stop = true;
         }
     }
@@ -248,6 +347,12 @@ void record() {
     cv::destroyWindow("Video");
 }
 
-void drawLine(Mat &picture, Point startPoint, Point endPoint) {
+void drawLine(Mat &picture, Point startPoint, Point endPoint)
+{
     line(picture, startPoint, endPoint, Scalar(255, 0, 0), 10);
+}
+
+void drawUnimportantLine(Mat &picture, Point startPoint, Point endPoint)
+{
+    line(picture, startPoint, endPoint, Scalar(0, 255, 0), 10);
 }
